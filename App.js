2988fb91c462bca359916adf105b16a5c33e3954
MaximusDaus <58691490+edrazi90@users.noreply.github.com>
2020-07-17 17:28:38 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,100 @@
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Image,StyleSheet, Text,TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+
+
+
+
+export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);//set the state to select image
+
+
+
+  //set up a function for picking up image
+  let openImagePickerAsnyc = async() =>{
+    let permissionResult =await ImagePicker.requestCameraRollPermissionsAsync();
+
+              if(permissionResult.granted===false){
+                alert("Permission to access album is required!");
+                return;
+              }
+              let pickerResult = await ImagePicker.launchImageLibraryAsync();
+              if (pickerResult.cancelled === true) {
+                return;
+              }
+          
+              setSelectedImage({ localUri: pickerResult.uri });
+            };
+            let openShareDialogAsync = async () => {
+              if (!(await Sharing.isAvailableAsync())) {
+                alert(`Uh oh, sharing isn't available on your platform`);
+                return;
+              }
+          
+              await Sharing.shareAsync(selectedImage.localUri);
+            }; 
+          
+            if (selectedImage !== null) {
+              return (
+                <View style={styles.container}>
+                  <Image
+                    source={{ uri: selectedImage.localUri }}style={styles.thumbnail}/>
+                    <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+                    <Text style={styles.buttonText}>Share this photo</Text>
+                  </TouchableOpacity>
+        
+                </View>
+              );
+            }
+   return (
+
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity onPress={openImagePickerAsnyc} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
+  } 
+});
